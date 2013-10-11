b25f3abc216d1a7d81ea5ead3b9078d06d4eb853
Sid Ferreira <sidney.ferreira@nextt.com.br>
2013-10-11 08:46:48 -0300

    Inicio Resize

--- server/php/index.php
+++ server/php/index.php
@@ -1,16 +1,8 @@
 <?php
-/*
- * jQuery File Upload Plugin PHP Example 5.14
- * https://github.com/blueimp/jQuery-File-Upload
- *
- * Copyright 2010, Sebastian Tschan
- * https://blueimp.net
- *
- * Licensed under the MIT license:
- * http://www.opensource.org/licenses/MIT
- */
-if(isset($_GET['resize'])) {
 
+
+if(isset($_GET['resize'])) {
+	var_dump($_REQUEST);
 } else {
 	error_reporting(E_ALL | E_STRICT);
 	require('UploadHandler.php');
